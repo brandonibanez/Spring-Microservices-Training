52c4de489c3054176057af05ecd30470b42dab81
brandonibanez <ibanezbrandonn@gmail.com>
2024-02-03 23:09:39 +0800

    Update & Delete with API
    -Update data on database
    -Delete data on database

--- section2/accounts/src/main/java/com/brandoncode/accounts/repository/AccountsRepository.java
+++ section2/accounts/src/main/java/com/brandoncode/accounts/repository/AccountsRepository.java
@@ -1,8 +1,9 @@
 package com.brandoncode.accounts.repository;
 
 import com.brandoncode.accounts.entity.Accounts;
-import com.brandoncode.accounts.entity.Customer;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,8 @@ import java.util.Optional;
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
