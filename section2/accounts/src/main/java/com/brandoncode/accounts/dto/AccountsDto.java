0b6e48a8a23520bae6307938ca6b4ef874a140b8
brandonibanez <ibanezbrandonn@gmail.com>
2024-02-04 21:25:58 +0800

    REST APIs documentation w/ springdoc-openapi
    -Uses Swagger UI
    -Improve documentation of REST Controller, and for each APIs methods
    -Provide documentation for DTO classes using @Schema

--- section2/accounts/src/main/java/com/brandoncode/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/brandoncode/accounts/dto/AccountsDto.java
@@ -1,20 +1,34 @@
 package com.brandoncode.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of Browns Bank account", example = "3454433243"
+    )
     private Long accountNumber;
 
     @NotEmpty(message = "AccountType can not be a null or empty")
+    @Schema(
+            description = "Account type of Browns Bank account", example = "Savings"
+    )
     private String accountType;
 
     @NotEmpty(message = "BranchAddress can not be a null or empty")
+    @Schema(
+            description = "Browns Bank branch address", example = "123 NewYork"
+    )
     private String branchAddress;
 
 }
