22ad3ff59332341489d16b11398ce8e2a7166d47
brandonibanez <ibanezbrandonn@gmail.com>
2024-04-07 16:47:22 +0800

    Custom Gateway Routing

--- section9/gatewayserver/src/main/java/com/brandoncode/gatewayserver/GatewayserverApplication.java
+++ section9/gatewayserver/src/main/java/com/brandoncode/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,37 @@ package com.brandoncode.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator brownsBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/brownsbank/accounts/**")
+						.filters( f -> f.rewritePath("/brownsbank/accounts/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/brownsbank/loans/**")
+						.filters( f -> f.rewritePath("/brownsbank/loans/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/brownsbank/cards/**")
+						.filters( f -> f.rewritePath("/brownsbank/cards/(?<segment>.*)","/${segment}")
+						.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
 	}
 
 }
