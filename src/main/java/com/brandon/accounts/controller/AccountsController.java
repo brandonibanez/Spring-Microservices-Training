90bafae0b468d435a6b9c8e4639b5c19beefe74c
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-22 23:26:44 +0800

    Create API with Exception Handling

--- src/main/java/com/brandon/accounts/controller/AccountsController.java
+++ src/main/java/com/brandon/accounts/controller/AccountsController.java
@@ -5,25 +5,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.brandon.accounts.constants.AccountsConstants;
 import com.brandon.accounts.dto.CustomerDto;
 import com.brandon.accounts.dto.ResponseDto;
+import com.brandon.accounts.service.IAccountsService;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping(path = "/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
+
+    private IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
     }
 
 }
