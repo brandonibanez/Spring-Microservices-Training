48d74959fa153bd5dd6d65b7012e893bfb5daf7e
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-22 17:15:44 +0800

    Entity & Repository Classes

---
+++ src/main/java/com/brandon/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.brandon.accounts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brandon.accounts.entity.Customer;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+}
