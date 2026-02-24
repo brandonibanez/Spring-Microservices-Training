6deb378cf1524d741e4910f705d19651d08b0ccf
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-24 21:25:50 +0800

    Update Audit Columns

--- src/main/java/com/brandon/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/brandon/accounts/service/impl/AccountsServiceImpl.java
@@ -40,8 +40,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
                     + customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Browns");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -58,8 +56,6 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Browns");
         return newAccount;
     }
 
