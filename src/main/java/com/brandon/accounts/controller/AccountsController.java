0d565ceb62ca56b2e25ca4e252ae08ae2a112c2b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-24 18:05:20 +0800

    Input Data Validations

--- src/main/java/com/brandon/accounts/controller/AccountsController.java
+++ src/main/java/com/brandon/accounts/controller/AccountsController.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService iAccountsService;
