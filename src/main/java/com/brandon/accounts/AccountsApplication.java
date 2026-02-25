0bb24c0c55d90581b43714a8f10b2844fa3fb40c
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-25 15:54:52 +0800

    Enhancing Documentation of APIs

--- src/main/java/com/brandon/accounts/AccountsApplication.java
+++ src/main/java/com/brandon/accounts/AccountsApplication.java
@@ -4,8 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Brandon Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Bandon Gandon",
+						email = "bandon@gandon.com",
+						url = "test"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "test"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Brandon Accounts microservice REST API Documentation",
+				url = "test"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
