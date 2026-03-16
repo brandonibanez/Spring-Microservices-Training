43acb7898f45bc1a181e7b3d7a2f336582c2f0b6
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-03-16 13:24:57 +0800

    Feign Client code changes

--- accounts/src/main/java/com/brandon/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/brandon/accounts/AccountsApplication.java
@@ -3,6 +3,7 @@ package com.brandon.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.brandon.accounts.dto.AccountsContactInfoDto;
@@ -14,6 +15,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @OpenAPIDefinition(
