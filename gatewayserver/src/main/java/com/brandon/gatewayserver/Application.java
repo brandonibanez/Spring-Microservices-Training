8f4dcd341d885b18151099a4d2644552f16b998b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-03-17 16:49:24 +0800

    Implementing Custom Routing using Spring Cloud Gateway

--- gatewayserver/src/main/java/com/brandon/gatewayserver/Application.java
+++ gatewayserver/src/main/java/com/brandon/gatewayserver/Application.java
@@ -2,12 +2,33 @@ package com.brandon.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r
+						.path("/brandon/accounts/**")
+						.filters(f -> f.rewritePath("/brandon/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(r -> r
+						.path("/brandon/cards/**")
+						.filters(f -> f.rewritePath("/brandon/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.route(r -> r
+						.path("/brandon/loans/**")
+						.filters(f -> f.rewritePath("/brandon/loans/(?<segment>.*)", "/${segment}"))
+						.uri("lb://LOANS"))
+				.build();
 	}
 
 }
