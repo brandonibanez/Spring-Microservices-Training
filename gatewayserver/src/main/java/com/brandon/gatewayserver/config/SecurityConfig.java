2316d66e85a639fd3d5a5245df8f6f2eea90dacf
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-03-29 18:33:56 +0800

    Change route mapping in gateway server

--- gatewayserver/src/main/java/com/brandon/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/brandon/gatewayserver/config/SecurityConfig.java
@@ -20,9 +20,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
-                .pathMatchers("/eazybank/cards/**").hasRole("CARDS")
-                .pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
+                .pathMatchers("/brandon/accounts/**").hasRole("ACCOUNTS")
+                .pathMatchers("/brandon/cards/**").hasRole("CARDS")
+                .pathMatchers("/brandon/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
                         .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
