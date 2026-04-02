0ce8d3277f056a0665896168d193a9d36488b5cc
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-04-02 23:44:21 +0800

    Update accounts microservice to stream & process events

--- accounts/src/main/java/com/brandon/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/brandon/accounts/service/IAccountsService.java
@@ -32,4 +32,11 @@ public interface IAccountsService {
      *         not
      */
     boolean deleteAccount(String mobileNumber);
+    
+    /**
+     *
+     * @param accountNumber - Long
+     * @return boolean indicating if the update of communication status is successful or not
+     */
+    boolean updateCommunicationStatus(Long accountNumber);
 }
