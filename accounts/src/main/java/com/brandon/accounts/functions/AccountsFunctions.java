ba995e1c379c92243f6df06f310a445f9788b9d9
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-04-14 14:39:10 +0800

    Use Kafka Streams

--- accounts/src/main/java/com/brandon/accounts/functions/AccountsFunctions.java
+++ accounts/src/main/java/com/brandon/accounts/functions/AccountsFunctions.java
@@ -1,12 +1,15 @@
 package com.brandon.accounts.functions;
 
 import com.brandon.accounts.service.IAccountsService;
+
+import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Configuration
 public class AccountsFunctions {
@@ -16,8 +19,18 @@ public class AccountsFunctions {
     @Bean
     public Consumer<Long> updateCommunication(IAccountsService accountsService) {
         return accountNumber -> {
-            log.info("Updating Communication status for the account number : " + accountNumber.toString());
+            log.info("Updating Communication status for the account number : " +
+                    accountNumber.toString());
             accountsService.updateCommunicationStatus(accountNumber);
+        };
+    }
+
+    @Bean
+    public Consumer<KStream<String, String>> analytics(IAccountsService accountsService) {
+        return input -> {
+            input.foreach((key, accountNumber) -> {
+                log.info("Kafka Streams: Processing account + 1: " + (Integer.parseInt(accountNumber) + 1));
+            });
         };
     }
 
