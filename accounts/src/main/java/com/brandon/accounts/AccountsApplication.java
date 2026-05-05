4fcb9d85fac632e23ee5d36c51b217496c2ed534
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-05 17:38:20 +0800

    Service discovery changes for Accounts

--- accounts/src/main/java/com/brandon/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/brandon/accounts/AccountsApplication.java
@@ -3,6 +3,7 @@ package com.brandon.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
